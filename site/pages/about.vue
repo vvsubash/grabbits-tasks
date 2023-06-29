423e50fddeb2221765dbb41146fdb8e5bf75d2e7
vvsubash <subash.venkat007@gmail.com>
2023-06-29 09:59:55 +0530

    added about page

---
+++ site/pages/about.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>
+            About Page
+        </h1>
+        <p>Header with signout is coming from nuxt layout's</p>
+
+        <NuxtLink to="/">Home Page</NuxtLink>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
--- site/pages/index.vue
+++ site/pages/index.vue
@@ -6,6 +6,9 @@
 
         <p>You have been succesfully logged in using a rest end point.</p>
 
+        <p>To check Layout Functonality</p>
+        <NuxtLink to="about" >About Page</NuxtLink>
+
     </div>
 </template>
 
